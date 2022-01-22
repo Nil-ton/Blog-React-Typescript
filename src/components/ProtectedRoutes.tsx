dfb1c0855f966f8e2abdced0c4d99ed1bb953ca9
Nilton Oliveira <nilton.oliveira0305@gmail.com>
2022-01-22 18:33:25 -0300

    logica de login para pagina de administração PRONTA

--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -1,32 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import api from "../api";
-
-
 
 interface Props {
     redirectTo: string
 }
 
-
 const ProtectedRoutes = ({ redirectTo }: Props) => {
     
 	const token = localStorage.getItem("token");
-	useEffect(() => {
-        
-		(async () => {
-			const {data} = await api.get("/authenticate/ensure", {
-				headers: {
-					authorization: `Bearer ${token}` 
-				}
-			});
-			if(data.Error) {
-				localStorage.clear();
-			}
-		})();
-
-	},[]);
-
 
 	return (
 		token ? <Outlet /> : <Navigate to={redirectTo}/>
--- src/pages/Adm.tsx
+++ src/pages/Adm.tsx
@@ -1,8 +1,20 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import api from "../api";
 
 const Adm = () => {
 	const token = localStorage.getItem("token");
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		api.get("/authenticate/ensure", {
+			headers: {
+				"Authorization": `Bearer ${token}`
+			}
+		})
+			.catch(err => err ? localStorage.clear() : navigate("/login"));
+	}, []);
+
 	return (
 		<>
 			<h1>Adm</h1>
